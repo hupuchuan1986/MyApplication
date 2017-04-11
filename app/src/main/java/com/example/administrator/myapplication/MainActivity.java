eaca73b0aa11c80fe07bba8342e3ce120b0c0ea7
hupuchuan1986 <lilong2533@gmail.com>
2017-04-11 13:39:34 +0800

    Initial commit

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.administrator.myapplication.utilites.MyTestUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyTestUtil util = new MyTestUtil();
+
+        TextView tv = (TextView) findViewById(R.id.myTextView);
+        tv.setText(util.getValue());
+
+        for (int counter = 0; counter < 10; counter++) {
+            Log.i("lilong", "value " + counter);
+        }
     }
 }
---
+++ app/src/main/java/com/example/administrator/myapplication/utilites/MyTestUtil.java
@@ -0,0 +1,11 @@
+package com.example.administrator.myapplication.utilites;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+
+public class MyTestUtil {
+    public String getValue() {
+        return "hello frome utilites class";
+    }
+}
